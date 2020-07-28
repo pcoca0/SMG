614472a34c383b3f1fce7f3344936374be69862e
pcoca1 <pcoca@lasegunda.com.ar>
2020-07-28 08:56:52 -0300

    client add to budget

--- src/app/pages/add-budget/add-budget.component.ts
+++ src/app/pages/add-budget/add-budget.component.ts
@@ -5,6 +5,8 @@ import { ProductService } from 'src/app/core/services/product.service';
 import { ICategoryItemResponse } from 'src/app/core/interfaces/responses/category.response';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { IClientItemResponse } from 'src/app/core/interfaces/responses/client.response';
+import { ClientService } from 'src/app/core/services/client.service';
 
 @Component({
   selector: 'app-add-budget',
@@ -13,6 +15,7 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class AddBudgetComponent implements OnInit {
 
+  today: number =  Date.now();
   totalizador: number;
   totalizadorParcial: number = 0.00;
   productos: Array<IProductItemResponse>;
@@ -20,18 +23,39 @@ export class AddBudgetComponent implements OnInit {
   bsModalRef: BsModalRef;
   listForm: FormGroup;
   element: IProductItemResponse;
+  clients: Array<IClientItemResponse>;
+
+  dropdownSetup: Object = {
+    displayKey:'razonSocial', //if objects array passed which key to be displayed defaults to description
+    search: true, //true/false for the search functionlity defaults to false,
+    height: 'auto', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
+    placeholder:'Selecciona', // text to be displayed when no item is selected defaults to Select,
+    customComparator: ()=>{}, // a custom function using which user wants to sort the items. default is undefined and Array.sort() will be used in that case,
+    //limitTo: options.length, // a number thats limits the no of options displayed in the UI similar to angular's limitTo pipe
+    moreText: 'Más', // text to be displayed whenmore than one items are selected like Option 1 + 5 more
+    noResultsFound: 'No se encontraron resultados!', // text to be displayed when no items are found while searching
+    searchPlaceholder:'Buscar', // label thats displayed in search input,
+    searchOnKey: 'descripcion', // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
+    clearOnSelection: false, // clears search criteria when an option is selected if set to true, default is false
+    inputDirection: 'ltr' // the direction of the search input can be rtl or ltr(default)
+  }
 
 
   constructor(
       private modalService: ModalService,
       private productService: ProductService,
+      private clientService: ClientService,
       private fB: FormBuilder
   ) { }
 
   ngOnInit() {
     this.productService.getProducts().subscribe(
-      resp => (this.productos = resp.data.productos,
-        console.log(resp.data.productos)))
+      resp => { this.productos = resp.data.productos,
+                console.log(resp.data.productos )} );
+    this.clientService.getClients().subscribe(
+      resp => {this.clients = resp.data.clientes,
+               console.log(this.clients)}
+    );
   }
 
   addBudget(){
